e7f55de543b84ccb796f46252121b1245165cd28
Marlon Hamaekers <marlonhamaekers@air-van-marlon.home>
2023-06-07 18:19:31 +0200

    add header effect

--- index.js
+++ index.js
@@ -4,8 +4,23 @@
 const navbarContainer = document.querySelector(".nav-container");
 const navItems = document.querySelectorAll(".nav-item");
 
-navbarContainer.addEventListener("mouseenter", function (e) {
-  if (e.target.classList.contains(".nav-item")) {
-    console.log("yes");
+
+navbarContainer.addEventListener("mouseover", function (e) {
+  if (e.target.classList.contains("nav-item")) {
+    const hover = e.target.dataset.nav;
+    const targetElement = document.querySelector(`.nav-item-${hover}`)
+    
+    navItems.forEach(el => el.classList.add("non-active"));
+    targetElement.classList.toggle("non-active");
+    
   }
 });
+
+navbarContainer.addEventListener("mouseleave", function (e) {
+  if (e.target.classList.contains("nav-item")) {
+    const hover = e.target.dataset.nav;
+    const targetElement = document.querySelector(`.nav-item-${hover}`)
+
+    navItems.forEach(el => el.classList.remove("non-active"));
+    
+}})
