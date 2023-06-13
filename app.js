3407786d478399090586675fc5e1d9f37fc5eed6
Amauri22 <mar.hamaekers@gmail.com>
2023-06-13 16:17:15 +0200

    basic function implementation

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
-"use strict"
+"use strict";
 
+/****Variables****/
+// HEADER VARIABLES
+const navContainer = document.querySelector(
+  ".header__nav-container__nav-items"
+);
+const navItems = document.querySelectorAll(".nav-item");
+
+// About variables
+const aboutImgContainer = document.querySelector(".about__grid-row__col--1");
+const aboutImages = document.querySelectorAll(".about__pictures");
+navContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // check if correct target
+  if (e.target.classList.contains("nav-item")) {
+    const clicked = e.target;
+    const dataNav = clicked.dataset.nav;
+    const element = document.getElementById(dataNav);
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+aboutImgContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("about__pictures")) {
+    console.log(e.target);
+  }
+});
